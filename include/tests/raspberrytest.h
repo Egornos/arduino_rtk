fed8bdb119597368100477877a291d42511cc3ac
Egornos <egornos193@gmail.com>
2026-05-19 00:31:20 +0300

    test for raspberry and arduino connection

--- include/tests/raspberrytest.h
+++ include/tests/raspberrytest.h
@@ -1,4 +1,6 @@
+#pragma once
 #include "Robot.h"
+#include "SerialReader.h"
 #include <PinChangeInterrupt.h>
 
 /*
@@ -14,7 +16,7 @@ void updateRWA() { robot.right_triggerA(); }
 void updateRWB() { robot.right_triggerB(); }
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     robot.reset();
     
     // Это триггеры, запускают функцию, при появлении напряжения на пине
@@ -35,54 +37,12 @@ unsigned long lastPrint = 0;
 const double DT = 0.02;
 const double PRINT_DT = 0.1;
 
-
-
-// Глобальные переменные
-float targetLeft = 0.0;
-float targetRight = 0.0;
-bool newData = false;
-String serialBuffer = "";
-
-void parseSerialData() {
-    while (Serial.available()) {
-        char c = Serial.read();
-        
-        if (c == '\n') {
-            // Конец строки - парсим
-            if (serialBuffer.length() > 0) {
-                int commaIndex = serialBuffer.indexOf(',');
-                if (commaIndex > 0) {
-                    String leftStr = serialBuffer.substring(0, commaIndex);
-                    String rightStr = serialBuffer.substring(commaIndex + 1);
-                    
-                    targetLeft = leftStr.toFloat();
-                    targetRight = rightStr.toFloat();
-                    
-                    // Ограничиваем значения
-                    targetLeft = constrain(targetLeft, -1.0, 1.0);
-                    targetRight = constrain(targetRight, -1.0, 1.0);
-                    
-                    newData = true;
-                }
-                serialBuffer = "";
-            }
-        } 
-        else if (c != '\r') { // Игнорируем carriage return
-            serialBuffer += c;
-            // Защита от переполнения буфера
-            if (serialBuffer.length() > 20) {
-                serialBuffer = "";
-            }
-        }
-    }
-}
-
-
 void loop() {
     unsigned long now = millis();
-    parseSerialData();
+    
     // Запускаем обновление одометрии и запускаем моторы, не чаще чем раз в DT секунд
     if (now - lastUpdate >= DT * 1000) {
+        readSerialData();
         lastUpdate = now;
         robot.update(); // Обновление информации
         robot.setMotors(targetLeft /*Левый*/, targetRight /*Правый*/); // Подача напряжений на моторы, от -1 до 1
